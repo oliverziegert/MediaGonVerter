7855823de9a2dc6834e90068b09f1f12bdb6a40c
Oliver Ziegert <oliver@pc-ziegert.de>
2022-03-24 20:54:09 +0100

    test

--- test.go
+++ test.go
@@ -1,13 +1,19 @@
 package main
 
-//func main() {
-//	img := m.NewImage("42", 1337)
-//	c := m.NewConversion(42, 42, false)
-//	img.Conversions = append(img.Conversions, c)
-//	v := reflect.ValueOf(*img)
-//
-//	for i := 0; i < v.NumField(); i++ {
-//		fmt.Printf("Field: %s\tValue: %v\tTag; %s\n", v.Type().Field(i).Name, v.Field(i).Interface(), v.Type().Field(i).Tag)
-//	}
-//
-//}
+import (
+	"fmt"
+	m "pc-ziegert.de/media_service/common/model"
+	"reflect"
+)
+
+func main() {
+	img := m.NewImage("42", 1337)
+	c := m.NewConversion(42, 42, false)
+	img.Conversions = append(img.Conversions, c)
+	v := reflect.ValueOf(*img)
+
+	for i := 0; i < v.NumField(); i++ {
+		fmt.Printf("Field: %s\tValue: %v\tTag; %s\n", v.Type().Field(i).Name, v.Field(i).Interface(), v.Type().Field(i).Tag)
+	}
+
+}
