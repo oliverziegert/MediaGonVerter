c66f7e29b5bc08bf1dfeea3cd4cd29d6ba08029c
Oliver Ziegert <O.Ziegert@dracoon.com>
2023-03-08 15:10:52 +0100

    use libvips for resizing images

--- worker/convert.go
+++ worker/convert.go
@@ -2,12 +2,11 @@ package worker
 
 import (
 	"fmt"
+	"github.com/davidbyttow/govips/v2/vips"
 	"github.com/streadway/amqp"
-	"image"
-	"image/jpeg"
 	"io"
+	"log"
 	"net/http"
-	"os"
 	"pc-ziegert.de/media_service/common/constant"
 	e "pc-ziegert.de/media_service/common/error"
 	l "pc-ziegert.de/media_service/common/log"
@@ -29,16 +28,6 @@ func Convert(in amqp.Delivery, cRes chan<- m.Image, tempDir string) {
 	i := m.Image{}
 	i.JsonToImage(in.Body)
 
-	tds := fmt.Sprintf("%v/%v-%d", tempDir, i.TenantUuid, i.NodeId)
-
-	if _, err := os.Stat(tds); os.IsNotExist(err) {
-		err := os.Mkdir(tds, os.ModePerm)
-		if err != nil {
-			l.Fatal(err.Error())
-		}
-	}
-	defer os.RemoveAll(tds)
-
 	hResp, err := http.Get(i.S3DownloadUrl)
 	if err != nil {
 		l.Fatal(err.Error())
@@ -46,25 +35,14 @@ func Convert(in amqp.Delivery, cRes chan<- m.Image, tempDir string) {
 	if hResp.StatusCode != http.StatusOK {
 		l.Fatal(err.Error())
 	}
+	defer hResp.Body.Close()
 
-	out, err := os.Create(tds + "/original")
-	if err != nil {
-		l.Fatal(err.Error())
-	}
-
-	// Write the body to file
-	n, err := io.Copy(out, hResp.Body)
-	if int64(i.NodeSize) != n {
-		l.Fatal("size mismatch")
-	}
-
-	hResp.Body.Close()
-	out.Close()
+	rawBody, err := io.ReadAll(hResp.Body)
 
 	for _, c := range i.Conversions {
 		if c.State != m.ConversionStateCached {
 			wg.Add(1)
-			go convertFile(&wg, &i, c, cRes, tds)
+			go convertFile(&wg, &i, c, cRes, &rawBody)
 		}
 	}
 
@@ -72,68 +50,41 @@ func Convert(in amqp.Delivery, cRes chan<- m.Image, tempDir string) {
 	return
 }
 
-func convertFile(wg *sync.WaitGroup, i *m.Image, c *m.Conversion, cRes chan<- m.Image, tds string) {
+func convertFile(wg *sync.WaitGroup, i *m.Image, c *m.Conversion, cRes chan<- m.Image, rawBody *[]byte) {
 	// call done when finished
 	defer wg.Done()
 
-	// open files
-	file, err := os.Open(tds + "/original")
+	// Open image with VIPS
+	iImage, err := vips.NewImageFromBuffer(*rawBody)
 	if err != nil {
-		err := e.NewError(e.ValIdInvalid, fmt.Sprintf("error opening filehandler of src image"))
-		l.Debug(err.StackTrace())
-		c.State = m.ConversionStateConversionError
-		cRes <- *i
-		return
-
-	}
-	defer file.Close()
-
-	newFilePath := fmt.Sprintf("%v/%dx%d-%t", tds, c.Width, c.Height, c.Crop)
-	outFile := openOrCreate(newFilePath)
-
-	// decode
-	imageData, _, err := image.Decode(file)
-	if err != nil {
-		err := e.NewError(e.ValIdInvalid, fmt.Sprintf("error decoding image"))
+		err := e.NewError(e.ValIdInvalid, fmt.Sprintf("error opening image"))
 		l.Debug(err.StackTrace())
 		c.State = m.ConversionStateConversionError
 		cRes <- *i
 		return
 	}
 
-	// resize
-	dst := Resize(imageData, ResizeConfig{
-		Width:           int(c.Width),
-		Height:          int(c.Height),
-		Crop:            c.Crop,
-		BackgroundColor: image.White,
-	})
-
-	// encode in jpeg
-	opt := jpeg.Options{Quality: 80}
-	err = jpeg.Encode(outFile, dst, &opt)
+	// Resize image
+	err = iImage.Thumbnail(int(c.Width), int(c.Height), vips.InterestingAttention)
 	if err != nil {
-		err := e.NewError(e.ValIdInvalid, fmt.Sprintf("error converting to jpeg"))
+		err := e.NewError(e.ValIdInvalid, fmt.Sprintf("error resizing image"))
 		l.Debug(err.StackTrace())
 		c.State = m.ConversionStateConversionError
 		cRes <- *i
 		return
 	}
 
-	// sync image to file
-	err = outFile.Close()
+	// Save resized image
+	ep := vips.NewJpegExportParams()
+	oImage, _, err := iImage.ExportJpeg(ep)
 	if err != nil {
-		err := e.NewError(e.ValIdInvalid, fmt.Sprintf("error writing image to file"))
-		l.Debug(err.StackTrace())
-		c.State = m.ConversionStateConversionError
-		cRes <- *i
-		return
+		log.Fatal(err.Error())
 	}
 
 	// upload image to file
-	err = UploadFile(newFilePath, c.S3UploadUrl, i.NodeType)
+	err = UploadFile(&oImage, c.S3UploadUrl, i.NodeType)
 	if err != nil {
-		err := e.WrapError(e.ValIdInvalid, fmt.Sprintf("Cant get tenant state"), err)
+		err := e.WrapError(e.ValIdInvalid, fmt.Sprintf("Cant upload image to s3"), err)
 		l.Debug(err.StackTrace())
 		c.State = m.ConversionStateConversionError
 		cRes <- *i
@@ -142,20 +93,4 @@ func convertFile(wg *sync.WaitGroup, i *m.Image, c *m.Conversion, cRes chan<- m.
 	c.State = m.ConversionStateCached
 	cRes <- *i
 	return
-}
-
-func openOrCreate(filename string) *os.File {
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		file, err := os.Create(filename)
-		if err != nil {
-			l.Fatal("error creating output file")
-		}
-		return file
-	} else {
-		file, err := os.Open(filename)
-		if err != nil {
-			l.Fatal("error opening output file")
-		}
-		return file
-	}
 }
